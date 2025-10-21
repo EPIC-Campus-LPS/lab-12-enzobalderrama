0fe9918ff9fb3bc3a8020deab9a1d0a7e7a47f4b
enzobalderrama <76264578+enzobalderrama@users.noreply.github.com>
2025-10-21 00:00:23 -0600

    Update

--- src/pack/SortingTester.java
+++ src/pack/SortingTester.java
@@ -4,21 +4,36 @@ public class SortingTester {
 
 	public static void main(String[] args) {
 		int[] arr = new int[100];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = (int)((Math.random()+5)*10);
+		}
+		//I changed the array length to test out the sheet
 		
-		long startTime = System.currentTimeMillis();
-		QuadSorts.bubbleSort(arr);
+		int[] arr1 = arr;
 		long startTime1 = System.currentTimeMillis();
-		QuadSorts.insertionSort(arr);
+		QuadSorts.bubbleSort(arr1);
+		long endTime1 = System.currentTimeMillis();
+		
+		int[] arr2 = arr;
 		long startTime2 = System.currentTimeMillis();
-		QuadSorts.selectionSort(arr);
+		QuadSorts.insertionSort(arr2);
+		long endTime2 = System.currentTimeMillis();
+		
+		int[] arr3 = arr;
 		long startTime3 = System.currentTimeMillis();
+		QuadSorts.selectionSort(arr3);
+		long endTime3 = System.currentTimeMillis();
+		
+		int[] arr4 = arr;
+		long startTime4 = System.currentTimeMillis();
+		QuadSorts.mergeSort(arr4);
+		long endTime4 = System.currentTimeMillis();
 		
 		
-		System.out.printf("%d%n",  startTime1 - startTime);
-
-		System.out.printf("%d%n",startTime2-startTime1);
-		
-		System.out.printf("%d%n",startTime3-startTime2);
+		System.out.printf("%d%n",  endTime1 - startTime1);
+		System.out.printf("%d%n",  endTime2 - startTime2);
+		System.out.printf("%d%n",  endTime3 - startTime3);
+		System.out.printf("%d%n",  endTime4 - startTime4);
 	}
 	
 
